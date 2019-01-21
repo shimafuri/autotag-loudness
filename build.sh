a9180c6607848de6d50462fae27f3a5ffefb80fc
Anonymous <user@example.com>
2019-01-21 21:39:12 +0900

    fix: Suppress whitespace/parens.

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-cpplint --filter=-whitespace/line_length,-whitespace/comments,-legal/copyright,-build/c++11 --quiet main.cpp && g++ main.cpp -I/usr/include/taglib -L/usr/lib/x86_64-linux-gnu -ltag -lstdc++fs
+cpplint --filter=-whitespace/line_length,-whitespace/comments,-whitespace/parens,-legal/copyright,-build/c++11 --quiet main.cpp && g++ main.cpp -I/usr/include/taglib -L/usr/lib/x86_64-linux-gnu -ltag -lstdc++fs
